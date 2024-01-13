cc037d096d39b6a464ed95d3518831093f2514f8
Christina Bernhardt <sozia76@web.de>
2024-01-14 00:14:20 +0100

    save local storage correctly

--- script.js
+++ script.js
@@ -1,89 +1,95 @@
+// Arrays definieren
 let posts = [
     {
-        'authorImg': '/img/dog2.jpg',
-        "authorName": 'Diddy Dackel',
-        "image": '/img/dog.jpg',
-        "comments": []
+        'authorImg': 'img/dog2.jpg',
+        'authorName': 'Diddy Dackel',
+        'image': 'img/dog.jpg',
+        'comments': []
     },
     {
-        'authorImg': '/img/dog2.jpg',
-        "authorName": 'Diddy Dackel',
-        "image": '/img/horse.jpg',
-        "comments": []
+        'authorImg': 'img/dog2.jpg',
+        'authorName': 'Diddy Dackel',
+        'image': 'img/horse.jpg',
+        'comments': []
     },
     {
-        'authorImg': '/img/raccoon.jpg',
-        "authorName": 'Willi Waschbär',
-        "image": '/img/sea.jpg',
-        "comments": []
+        'authorImg': 'img/raccoon.jpg',
+        'authorName': 'Willi Waschbär',
+        'image': 'img/sea.jpg',
+        'comments': []
     },
     {
-        'authorImg': '/img/owl.jpg',
-        "authorName": 'Eule Beule',
-        "image": '/img/job.jpg',
-        "comments": []
+        'authorImg': 'img/owl.jpg',
+        'authorName': 'Eule Beule',
+        'image': 'img/job.jpg',
+        'comments': []
     },
 ];
 
 function render() {
-    // loadFromLocalStorage();
-    let content = document.getElementById('content');
+    loadFromLocalStorage();
+    const content = document.getElementById('content');
     content.innerHTML = '';
-    // loadFromLocalStorage();
-
     for (let i = 0; i < posts.length; i++) {
-        const post = posts[i];
-        content.innerHTML += /* html */`           
-        
-            <div class="card">
-                <div class="author">
-                 <img class="round" src="${post['authorImg']}">
-                 <div class="name">${post['authorName']}</div>
-                </div>
-            <img class="imagePost" src="${post['image']}">
-            <div>
-    
-            <img onclick="like(${i})" class="heart" id="heart${i}" src="/img/herz.png"> 
-            </div>
-
-            <div>${post['comments']}</div>
-            <input class="input" id="input${i}"><button class="button" onclick="addComment(${i})">Senden</button>
-            </div>
-        `;
-save()
-loadFromLocalStorage();
+        content.innerHTML += generatePostHTML(posts, i);
+        for (let j = 0; j < posts[i].comments.length; j++) {
+            const comments = document.getElementById(`comments${i}`);
+            comments.innerHTML += renderPost(posts, i, j)
+        }
     }
 }
+
+function renderPost(array, index, index2) {
+    return  /*html*/`
+    <span>
+        ${array[index]['comments'][index2]}
+    </span>  
+    `
+}
+
+function generatePostHTML(array, i) {
+    return  /*html*/`  
+    <div class="card">
+        <div class="author">
+            <img class="round" src="${array[i]['authorImg']}">
+                <div class="name">${array[i]['authorName']}</div>
+        </div>
+            <img class="imagePost" src="${array[i]['image']}">
+        <div>
+            <img onclick="like(${i})" class="heart" id="heart${i}" src="img/herz.png">
+        </div>
+        <div id="comments${i}">
+        </div>
+        <input class="input" id="input${i}"><button class="button" onclick="addComment(${i})">Senden</button>
+    </div>
+            `;
+}
+
 // Hinzufügen neuer Kommentare
 function addComment(index) {
     let input = document.getElementById(`input${index}`);
-    posts[index]['comments'].push(input.value);
-    render();
-    save();
+    if (document.getElementById(`input${index}`).value === '') {
+        return
+    } else {
+        posts[index]['comments'].push(`Du: ${input.value} <br>`);
+    }
     input.value = '';
+    saveToLocalStorage();
+    render();
 }
 
-// Speichern neuer Kommentare im Local Storage
-// function save(){
-//     let postsAsText = JSON.stringify(`input${index}`);
-//     localStorage.setItem('titles', postsAsText);
-//       }
-
-function save() {
+// Speichern im Local Storage
+function saveToLocalStorage() {
     let postsAsText = JSON.stringify(posts);
     localStorage.setItem('posts', postsAsText);
 }
 
+// Laden aus Local Storage
 function loadFromLocalStorage() {
-    let postsAsText = localStorage.getItem('posts');
-
-    if (postsAsText) {
-        posts = JSON.parse(postsAsText);
-    }
+    posts = JSON.parse(localStorage.getItem('posts'));
 }
 
 // Funktion like
 function like(index) {
-    document.getElementById(`heart${index}`).src = "/img/herzrot.png";
-
+    document.getElementById(`heart${index}`).src = "img/herzrot.png";
 }
